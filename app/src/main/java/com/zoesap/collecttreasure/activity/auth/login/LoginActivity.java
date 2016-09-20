b58570e7a8daa1499c29b50445cacbb1ed25dc5d
LoveLittleTeemo <380591858@qq.com>
2016-09-20 16:30:58 +0800

    9.20 增加界面

---
+++ app/src/main/java/com/zoesap/collecttreasure/activity/auth/login/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.zoesap.collecttreasure.activity.auth.login;
+
+import com.zoesap.collecttreasure.base.BaseActivity;
+
+/**
+ * 登陆
+ * 作者：Dubei on 2016/9/20 16:27
+ * 邮箱：370318201@qq.com
+ */
+public class LoginActivity extends BaseActivity {
+}
---
+++ app/src/main/java/com/zoesap/collecttreasure/activity/auth/register/RegisterActivity.java
@@ -0,0 +1,10 @@
+package com.zoesap.collecttreasure.activity.auth.register;
+
+import com.zoesap.collecttreasure.base.BaseActivity;
+
+/**
+ * 作者：Dubei on 2016/9/20 16:28
+ * 邮箱：370318201@qq.com
+ */
+public class RegisterActivity extends BaseActivity {
+}
